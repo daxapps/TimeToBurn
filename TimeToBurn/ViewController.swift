4475b0a0ddcf8c2ab01169e3b0b16e9b641bd22b
daxapps <daxapps777@gmail.com>
2017-02-25 11:50:46 -0600

    Setup

--- TimeToBurn/ViewController.swift
+++ TimeToBurn/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    var coords = CLLocationCoordinate2D(latitude: 40, longitude: 40)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print("DAX: location changed")
+        
+        if status == .authorizedWhenInUse {
+            getLocation()
+        } else if status == .denied {
+            let alert = UIAlertController(title: "Error", message: "Go to settings and allow this app to access you location!", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func getLocation() {
+        if let loc = locationManager.location?.coordinate {
+            coords = loc 
+        }
+    }
 
 }
+
+
+
+
+
+
 
